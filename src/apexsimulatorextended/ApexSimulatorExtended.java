47b479fb5794dc03e03f2acad7b395d1627db498
HarshadLoya <harshadloya@gmail.com>
2016-12-10 21:58:01 -0500

    YO

--- src/apexsimulatorextended/ApexSimulatorExtended.java
+++ src/apexsimulatorextended/ApexSimulatorExtended.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package apexsimulatorextended;
 
 import common.CircularQueue;
@@ -21,10 +16,6 @@ import java.util.Objects;
 import java.util.Scanner;
 import java.util.Queue;
 
-/**
- * 
- * @author Paritosh Fulara
- */
 public class ApexSimulatorExtended {
 
 	/**
